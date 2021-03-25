8495f0699746d99d42840180bf639a0d0a667f09
Marcos <marcos@marcos.es>
2021-03-25 05:12:23 +0100

    Testing Exceptions are thrown

--- PracticaPHPUnit/tests/unit/BookCar/BookCarTest.php
+++ PracticaPHPUnit/tests/unit/BookCar/BookCarTest.php
@@ -9,18 +9,24 @@ use Model\Booking;
 use Service\CarFinder;
 use Model\Car;
 use Service\DbConnection;
+use UseCase\CarNotAvailableException;
+use UseCase\CarNotFoundException;
+use UseCase\MinorsCannotBookCarsException;
 
-class BookCarTest extends TestCase {
+class BookCarTest extends TestCase
+{
 
     /**
      * @test
      */
-    public function adultsCanBookAvailableCars() {
-        
+    public function adultsCanBookAvailableCars()
+    {
+
         $userStub = $this->createStub(User::class);
         $userStub->method('getId')
             ->willReturn(1);
-
+        $userStub->method('isAnAdult')
+            ->willReturn(true);
 
         $dbConnectionStub = $this->createStub(DbConnection::class);
         $dbConnectionStub->method('insert')
@@ -37,13 +43,69 @@ class BookCarTest extends TestCase {
                 $carStub
             );
 
-        $bookCarUseCase  = new BookCar($carFinderStub, $dbConnectionStub);
+        $bookCarUseCase = new BookCar($carFinderStub, $dbConnectionStub);
         $booking = $bookCarUseCase->execute(
-            $userStub, 
+            $userStub,
             1
         );
 
         $this->assertInstanceOf(Booking::class, $booking);
+    }
+
+    /**
+     * @test
+     */
+    public function adultsCantBookUnavailableCars()
+    {
+        $this->expectException(CarNotAvailableException::class);
+
+        $userDummy = $this->createStub(User::class);
+
+        $dbConnectionDummy = $this->createStub(DbConnection::class);
+
+
+        $carStub = $this->createStub(Car::class);
+        $carStub->method('isAvailable')
+            ->willReturn(false);
+
+        $carFinderStub = $this->createStub(CarFinder::class);
+
+        $carFinderStub->method('find')
+            ->willReturn(
+                $carStub
+            );
+
+        $bookCarUseCase = new BookCar($carFinderStub, $dbConnectionDummy);
+
+        $bookCarUseCase->execute(
+            $userDummy,
+            1
+        );
+
+    }
+
+    /**
+     * @test
+     */
+    public function adultsCantBookNonExistentCars()
+    {
+        $this->expectException(CarNotFoundException::class);
+
+        $userDummy = $this->createStub(User::class);
+        $dbConnectionDummy = $this->createStub(DbConnection::class);
+
+        $carFinderStub = $this->createStub(CarFinder::class);
+        $carFinderStub->method('find')
+            ->willThrowException(
+                new CarNotFoundException()
+            );
+
+        $bookCarUseCase = new BookCar($carFinderStub, $dbConnectionDummy);
+
+        $bookCarUseCase->execute(
+            $userDummy,
+            1
+        );
     }
 
 }
